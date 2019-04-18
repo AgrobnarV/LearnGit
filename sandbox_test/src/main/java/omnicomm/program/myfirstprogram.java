a8e0508fc35dfe042624435597c947552c7da43f
avoronin <voron86618@gmail.com>
2019-04-18 22:55:02 +0300

    Добавил формулу для площади квадрата

--- sandbox_test/src/main/java/omnicomm/program/myfirstprogram.java
+++ sandbox_test/src/main/java/omnicomm/program/myfirstprogram.java
@@ -3,7 +3,11 @@ package omnicomm.program;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("hello,world");
+    System.out.println("hello," + "world"); // выводим строку hello,world
+
+    int l = 2; //значение стороны квадрата
+    int s = l * l; //формула расчета площади
+    System.out.println("Площадь квадрата со стороной " + l +  " = " + s); //считаем площадь квадрата по сторонам
   }
 
 }
