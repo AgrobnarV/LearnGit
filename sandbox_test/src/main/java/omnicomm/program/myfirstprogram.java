83321f0004c61af3eb8512e86320ec53d241b00b
avoronin <voron86618@gmail.com>
2019-04-18 21:32:44 +0300

    Изменил название метода, изменил название в билдере

--- sandbox_test/src/main/java/omnicomm/program/myfirstprogram.java
+++ sandbox_test/src/main/java/omnicomm/program/myfirstprogram.java
@@ -2,7 +2,7 @@ package omnicomm.program;
 
 public class MyFirstProgram {
 
-  public static void mainMethod(String[] args) {
+  public static void main(String[] args) {
     System.out.println("hello,world");
   }
 
