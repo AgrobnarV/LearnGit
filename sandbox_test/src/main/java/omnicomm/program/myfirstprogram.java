036a64da0ad3895508683b3d501cae50553aa042
avoronin <voron86618@gmail.com>
2019-04-28 17:27:40 +0300

    Оптимизировал код для функций квадрата или прямоугольника

--- sandbox_test/src/main/java/omnicomm/program/myfirstprogram.java
+++ sandbox_test/src/main/java/omnicomm/program/myfirstprogram.java
@@ -6,20 +6,12 @@ public class MyFirstProgram {
     hello("Andrey"); // вызвать имя пользователя
 
     Square s = new Square(5); //вызов функкции квадрата
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s)); // считаем площадь квадрата по стороне
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area()); // считаем площадь квадрата по стороне
 
     Rectangle r = new Rectangle(4,6); // вызов функции прямоугольника
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r)); //считаем площадь прямоугольника по сторонам
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area()); //считаем площадь прямоугольника по сторонам
   }
   public static void hello (String user) { // приветствие пользователя
     System.out.println("hello," + user + "."); // вывод строки приветствия
-  }
-
-  public static double area (Square s) { // расчет площади из стороны квадрата
-    return s.l * s.l; // формула расчета площади квадрата
-  }
-  public static double area (Rectangle r) { // расчет площади из сторон прямоугольника
-    return r.a * r.b; // формула расчета площади прямоугольника
-
   }
 }
