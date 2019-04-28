a7c8334df88d43a1dffea559b3ca5e8a7cef5488
avoronin <voron86618@gmail.com>
2019-04-28 22:05:19 +0300

    Добавил комментарии

--- sandbox_test/src/main/java/omnicomm/program/MySecondProgram.java
+++ sandbox_test/src/main/java/omnicomm/program/MySecondProgram.java
@@ -5,12 +5,12 @@ public class MySecondProgram {
   public static void main(String[] args) {
     dz ("Задание №2: Потренироваться использовать функции, классы, объекты и методы");
 
-    Point p1 = new Point(15,10,20,5); //
-    System.out.println("Точка 1 с координатами " + p1.x1 + " и " + p1.y1);
-    System.out.println("Точка 2 с координатами " + p1.x2 + " и " + p1.y2);
-    System.out.println("Расстояние между точками = " + p1.Distanse ());
+    Point p1 = new Point(15,10,20,5); // ввод вручную значений координат
+    System.out.println("Точка 1 с координатами " + p1.x1 + " и " + p1.y1); // вывод 1 точки
+    System.out.println("Точка 2 с координатами " + p1.x2 + " и " + p1.y2); // вывод 2 точки
+    System.out.println("Расстояние между точками = " + p1.Distanse ()); // вывод расстояния
     }
 
-  public static void dz (String dz2) { // приветствие пользователя
-    System.out.println(dz2); // вывод строки приветствия
+  public static void dz (String dz2) {
+    System.out.println(dz2); // вывод строки с дз
   }  }
