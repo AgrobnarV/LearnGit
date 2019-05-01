9cb73036e3bc093140a8a488a280e9517c0ed320
avoronin <voron86618@gmail.com>
2019-05-01 19:48:27 +0300

    Обновил тесты, добавил округление до целого

--- sandbox_test/src/test/java/omnicomm/program/PointTests.java
+++ sandbox_test/src/test/java/omnicomm/program/PointTests.java
@@ -5,13 +5,27 @@ import org.testng.annotations.Test;
 
 public class PointTests {
   @Test
-  public void TestMethod1() {
-    Point p1 = new Point (1,2);
-    Assert.assertEquals(p1.distance(p1),2.0);
+  public void TestCommonPoint() {
+    Point a = new Point (25,16);
+    Point b = new Point (1,15);
+    Assert.assertEquals(Math.round(b.getDistance(a)* 100.0 / 100.0),24);
   }
   @Test
-  public void TestMethod2() {
-    Point p2 = new Point (1,2);
-    Assert.assertEquals(p2.distance(p2),2.0);
+  private void TestHighDiapozon() {
+    Point a = new Point (1,999);
+    Point b = new Point (999,0);
+    Assert.assertEquals(Math.round(b.getDistance(a)* 100.0 / 100.0),1412);
+  }
+  @Test
+  private void TestWithFirstNullPoint() {
+    Point a = new Point (0,0);
+    Point b = new Point (2,2);
+    Assert.assertEquals(Math.round(b.getDistance(a)* 100.0 / 100.0),3);
+  }
+  @Test
+  private void TestWithSecondNullPoint() {
+    Point a = new Point (5,5);
+    Point b = new Point (0,0);
+    Assert.assertEquals(Math.round(b.getDistance(a)* 100.0 / 100.0),7);
   }
 }
