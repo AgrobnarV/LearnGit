3882d10571d338ee95fc7e67ed8c191fa8e6234c
avoronin <voron86618@gmail.com>
2019-05-01 19:16:45 +0300

    Добавил округление до тысячных

--- sandbox_test/src/main/java/omnicomm/program/MySecondProgram.java
+++ sandbox_test/src/main/java/omnicomm/program/MySecondProgram.java
@@ -6,6 +6,6 @@ public class MySecondProgram {
 
     Point a = new Point(25, 16);
     Point b = new Point(1, 15);
-    System.out.println("Расстояние между точками: "+ b.getDistance(a));
+    System.out.println("Расстояние между точками: "+ Math.round(b.getDistance(a) * 1000.0) / 1000.0);
   }
 }
