a86ff63e55d6d4d97145e3a415b080448845e4c3
avoronin <voron86618@gmail.com>
2019-05-03 17:53:59 +0300

    Обновил тесты на основе EqualsTrue/False
    Исключил округление до сотых в виду нецелесообразности
    Сделал проверку на граничные значения чисел

--- sandbox_test/src/test/java/omnicomm/program/PointTests.java
+++ sandbox_test/src/test/java/omnicomm/program/PointTests.java
@@ -3,29 +3,43 @@ package omnicomm.program;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.math.RoundingMode;
+
 public class PointTests {
   @Test
   public void TestCommonPoint() {
-    Point a = new Point (25,16);
-    Point b = new Point (1,15);
-    Assert.assertEquals(Math.round(b.getDistance(a)* 100.0 / 100.0),24);
+    Point a = new Point(25, 16);
+    Point b = new Point(1, 15);
+    Assert.assertEquals(24.020824298928627, b.getDistance(a));
+    //  Assert.assertEquals(b.getDistance(a),4);
   }
   @Test
+  public void TestPointNotNull() {
+    Point a = new Point (1,0);
+    Point b = new Point (1,0);
+    Assert.assertNotNull( b.getDistance(a));
+  }
+ @Test
   private void TestHighDiapozon() {
-    Point a = new Point (1,999);
-    Point b = new Point (999,0);
-    Assert.assertEquals(Math.round(b.getDistance(a)* 100.0 / 100.0),1412);
+    Point a = new Point (1,0);
+    Point b = new Point (1,0);
+    Assert.assertTrue(b.getDistance(a) == 0.0);
+    Assert.assertFalse(b.getDistance(a) == 1.5);
   }
   @Test
   private void TestWithFirstNullPoint() {
     Point a = new Point (0,0);
     Point b = new Point (2,2);
-    Assert.assertEquals(Math.round(b.getDistance(a)* 100.0 / 100.0),3);
+    Assert.assertEquals(b.getDistance(a),2.8284271247461903);
+    Assert.assertTrue(Math.round(b.getDistance(a)) == 3.0);
+    Assert.assertFalse(Math.round(b.getDistance(a)) == 2.82);
   }
   @Test
   private void TestWithSecondNullPoint() {
     Point a = new Point (5,5);
     Point b = new Point (0,0);
-    Assert.assertEquals(Math.round(b.getDistance(a)* 100.0 / 100.0),7);
+    Assert.assertEquals(b.getDistance(a),7.0710678118654755);
+    Assert.assertTrue(Math.round(b.getDistance(a)) == 7.0);
+    Assert.assertFalse(Math.round(b.getDistance(a)) == 7.1);
   }
 }
