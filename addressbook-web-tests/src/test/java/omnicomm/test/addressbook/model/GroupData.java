10a9a91cd49a90abe31911edf212280b9c64b36a
Andrew Voronin <46493509+AgrobnarV@users.noreply.github.com>
2019-05-29 23:07:15 +0300

    Добавлена проверка на списки групп целиком для удаления групп

--- addressbook-web-tests/src/test/java/omnicomm/test/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/omnicomm/test/addressbook/model/GroupData.java
@@ -1,5 +1,7 @@
 package omnicomm.test.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupData {
   private final String gname;
   private final String gheader;
@@ -9,6 +11,26 @@ public class GroupData {
     this.gname = gname;
     this.gheader = gheader;
     this.gfooter = gfooter;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "gname='" + gname + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(gname, groupData.gname);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(gname);
   }
 
   public String getGname() {
