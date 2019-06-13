c920893e80715e51bebb812d1bcb7606723e4559
Andrew Voronin <46493509+AgrobnarV@users.noreply.github.com>
2019-06-13 23:11:59 +0300

    добавил опции ком. строке при генерации даннных для групп и контактов
    Кол-во групп - 20
    Кол-во контактов - 10

--- addressbook-web-tests/src/test/java/omnicomm/test/addressbook/appmanager/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/omnicomm/test/addressbook/appmanager/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package omnicomm.test.addressbook.appmanager.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import omnicomm.test.addressbook.model.ContactData;
 
 import java.io.File;
@@ -10,16 +13,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
+
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+
   public static void main (String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
-
-    List<ContactData> contacts = generateContacts(count);
-    save(contacts, file);
-
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try{
+      jCommander.parse(args);
+    } catch (ParameterException en){
+      jCommander.usage();
+      return;
+    }
+    generator.run();
   }
 
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, new File(file));
+  }
+
+  private void save(List<ContactData> contacts, File file) throws IOException {
     System.out.println(new File(".").getAbsoluteFile());
     Writer writer = new FileWriter(file);
     for(ContactData contact : contacts) {
@@ -35,7 +54,7 @@ public class ContactDataGenerator {
 
   }
 
-  private static List<ContactData> generateContacts(int count) {
+  private List<ContactData> generateContacts(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++){
       contacts.add(new ContactData()
