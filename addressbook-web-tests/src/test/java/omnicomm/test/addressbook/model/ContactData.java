525080cce855265d22a38b34e753ccc247cc033a
Andrew Voronin <46493509+AgrobnarV@users.noreply.github.com>
2019-06-23 14:41:46 +0300

    Добавил поддержку БД Hibernate ORM и класс проверки подключения к БД

--- addressbook-web-tests/src/test/java/omnicomm/test/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/omnicomm/test/addressbook/model/ContactData.java
@@ -3,35 +3,81 @@ package omnicomm.test.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 
 public class ContactData {
+
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
+
   @Expose
+  @Column(name = "firstname")
   private String firstname;
-  @Expose
+
+  @Column(name = "lastname")
   private String lastname;
+
   @Expose
+  @Column(name = "address")
+  @Type(type = "text")
   private String address;
+
   @Expose
+  @Column(name = "home")
+  @Type(type = "text")
   private String telephone;
+
+  @Expose
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobilePhone;
+
+  @Column(name = "work")
+  @Type(type = "text")
   private String workPhone;
+
+  @Expose
+  @Transient
   private String allPhones;
+
   @Expose
+  @Column(name = "email")
+  @Type(type = "text")
   private String email;
+
+  @Column(name = "email2")
+  @Type(type = "text")
   private String emailHome;
+
+  @Column(name = "email3")
+  @Type(type = "text")
   private String emailWork;
+
+  @Expose
+  @Transient
   private String allEmails;
+
   @Expose
+  @Transient
   private String group;
+
   @Expose
+  @Column(name = "homepage")
+  @Type(type = "text")
   private String homepage;
+
+  @Column(name = "photo")
+  @Type(type = "text")
   private File photo;
 
   public ContactData withId(int id) {
---
+++ addressbook-web-tests/src/test/java/omnicomm/test/addressbook/tests/HbConnectionTest.java
@@ -0,0 +1,54 @@
+package omnicomm.test.addressbook.tests;
+
+import omnicomm.test.addressbook.model.GroupData;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class HbConnectionTest {
+
+  private SessionFactory sessionFactory;
+
+  @BeforeClass
+  protected void setUp() throws Exception {
+    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    try {
+      sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    } catch (Exception e) {
+      e.printStackTrace();
+      StandardServiceRegistryBuilder.destroy(registry);
+    }
+  }
+
+  @Test
+  public void testHbConnection() {
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<GroupData> result = session.createQuery("from ContactData where deprecated = '0000-00-00 00:00:00'").list();
+    for (GroupData group : result) {
+      System.out.println(group);
+    }
+    session.getTransaction().commit();
+    session.close();
+  }
+
+  /*@Test
+    public void testHbConnectionTestContact(){
+        Session session = sessionFactory.openSession();
+        sessionFactory.openSession();
+        session.beginTransaction();
+        List<ContactData> result = session.createQuery( "from ContactData" ).list();
+        for ( ContactData contact : result) {
+            System.out.println(contact);
+        }
+        session.getTransaction().commit();
+        session.close();
+    }*/
+}
