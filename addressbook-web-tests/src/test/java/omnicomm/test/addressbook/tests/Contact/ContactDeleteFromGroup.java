338488e450271042dbf7aa0fa4ecce651867e21f
Andrew Voronin <46493509+AgrobnarV@users.noreply.github.com>
2019-07-18 23:23:59 +0300

    Дз 16 Заменил GroupPage на Homepage

--- addressbook-web-tests/src/test/java/omnicomm/test/addressbook/tests/Contact/ContactDeleteFromGroup.java
+++ addressbook-web-tests/src/test/java/omnicomm/test/addressbook/tests/Contact/ContactDeleteFromGroup.java
@@ -18,11 +18,11 @@ public class ContactDeleteFromGroup extends TestBase {
   @BeforeMethod
   public void ensurePreconditions() {
     if(app.db().groups().size() == 0){
-      app.goTo().groupPage();
+      app.goTo().homePage();
       app.group().create(new GroupData().withGname("test 1").withGheader("header").withGfooter("footer"));
     }
     if(app.db().contacts().size() == 0){
-      app.goTo().groupPage();
+      app.goTo().homePage();
       ContactData newContact = new ContactData()
               .withFirstname("name 1")
               .withLastname("lastname 1")
@@ -30,7 +30,7 @@ public class ContactDeleteFromGroup extends TestBase {
               .withTelephone("+380971785225")
               .withEmail("email 1");
       app.contact().createContact(newContact);
-      app.goTo().groupPage();
+      app.goTo().homePage();
     }
     Groups groups = app.db().groups();
     Contacts contacts = app.db().contacts().stream().filter(contact -> contact.getGroups().size() < groups.size()).collect(Collectors.toCollection(Contacts::new));
@@ -43,10 +43,10 @@ public class ContactDeleteFromGroup extends TestBase {
               .withTelephone("+380971785225")
               .withEmail("email 1");
       app.contact().createContact(newContact);
-      app.goTo().groupPage();
+      app.goTo().homePage();
       contacts = new Contacts().withAdded(newContact);
     }
-    app.goTo().groupPage();
+    app.goTo().homePage();
     ContactData modify = contacts.iterator().next();
     groups.removeAll(modify.getGroups());
     app.contact().addToGroup(modify, groups.iterator().next());
@@ -68,14 +68,14 @@ public class ContactDeleteFromGroup extends TestBase {
               .withEmail("email 1")
               .inGroup(groups.iterator().next());
       app.contact().createContact(newContact);
-      app.goTo().groupPage();
+      app.goTo().homePage();
       contacts = new Contacts().withAdded(newContact);
     }
 
     ContactData contact = contacts.iterator().next();
-    app.goTo().groupPage();
+    app.goTo().homePage();
     app.contact().removeGroupFrom(contact);
-    app.goTo().groupPage();
+    app.goTo().homePage();
     ContactData contactWithoutGroup = app.db().contacts().stream().filter(c -> c.getId() == contact.getId()).collect(Collectors.toList()).iterator().next();
     assertThat(contactWithoutGroup.getGroups(), equalTo(contact.getGroups().withoutGroup(contact.getGroups().iterator().next())));
   }
