80e7d7d6d5455a912ec4ecf3ae9a532992fe6779
Andrew Voronin <46493509+AgrobnarV@users.noreply.github.com>
2019-08-05 22:07:00 +0300

    SOAP: простой пример

---
+++ soap-sample/src/test/java/ru/omnicomm/soap/GeoIpServiceTests.java
@@ -0,0 +1,21 @@
+package ru.omnicomm.soap;
+
+import com.lavasoft.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("77.243.98.10");
+    assertEquals(ipLocation, "<GeoIP><Country>RU</Country><State>48</State></GeoIP>");
+  }
+
+  @Test
+  public void testInvalidIp() {
+    String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("77.243.98.xxx");
+    assertEquals(ipLocation, "<GeoIP><Country>RU</Country><State>48</State></GeoIP>");
+  }
+}
