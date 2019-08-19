3e4912237239c0ab3023f3e05b33c99f3789aa09
Andrew Voronin <46493509+AgrobnarV@users.noreply.github.com>
2019-08-19 22:40:50 +0300

    добавил запуск с платформы

--- addressbook-web-tests/src/test/java/omnicomm/test/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/omnicomm/test/addressbook/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package omnicomm.test.addressbook.appmanager;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -48,8 +49,8 @@ public class ApplicationManager {
       } else {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName(browser);
+        capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
         wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
-
         wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
